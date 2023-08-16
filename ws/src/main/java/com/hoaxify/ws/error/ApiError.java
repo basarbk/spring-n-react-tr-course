e799a332c72f4160662b0defa1b998abf35fc271
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-16 23:08:36 +0300

    0301-validate-username

---
+++ ws/src/main/java/com/hoaxify/ws/error/ApiError.java
@@ -0,0 +1,59 @@
+package com.hoaxify.ws.error;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApiError {
+
+    private int status;
+
+    private String message;
+
+    private String path;
+
+    private long timestamp = new Date().getTime();
+
+    private Map<String, String> validationErrors = new HashMap<>();
+
+    public Map<String, String> getValidationErrors() {
+        return validationErrors;
+    }
+
+    public void setValidationErrors(Map<String, String> validationErrors) {
+        this.validationErrors = validationErrors;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+}
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,10 +1,15 @@
 package com.hoaxify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericMessage;
 
 @RestController
@@ -14,9 +19,20 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user){
+    ResponseEntity<?> createUser(@RequestBody User user){
+        if(user.getUsername() == null || user.getUsername().isEmpty()) {
+            ApiError apiError = new ApiError();
+            apiError.setPath("/api/v1/users");
+            apiError.setMessage("Validation error");
+            apiError.setStatus(400);
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "Username cannot be null");
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);
+        }
+
         userService.save(user);
-        return new GenericMessage("User is created");
+        return ResponseEntity.ok(new GenericMessage("User is created"));
     }
     
 }
