94a5463a3cbeaa1de5d49f9d4e2385dd8f963cfd
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-16 23:35:49 +0300

    0304-validate-email

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -20,13 +20,20 @@ public class UserController {
 
     @PostMapping("/api/v1/users")
     ResponseEntity<?> createUser(@RequestBody User user){
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation error");
+        apiError.setStatus(400);
+        Map<String, String> validationErrors = new HashMap<>();
         if(user.getUsername() == null || user.getUsername().isEmpty()) {
-            ApiError apiError = new ApiError();
-            apiError.setPath("/api/v1/users");
-            apiError.setMessage("Validation error");
-            apiError.setStatus(400);
-            Map<String, String> validationErrors = new HashMap<>();
             validationErrors.put("username", "Username cannot be null");
+        }
+
+        if(user.getEmail() == null || user.getEmail().isEmpty()) {
+            validationErrors.put("email", "E-mail cannot be null");
+        }
+
+        if(validationErrors.size() > 0) {
             apiError.setValidationErrors(validationErrors);
             return ResponseEntity.badRequest().body(apiError);
         }
