f98f201ba8fc98c670fd45a7c99665749cd9daca
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-16 22:08:57 +0300

    0208-hashing-password

--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.hoaxify.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +11,10 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
     
