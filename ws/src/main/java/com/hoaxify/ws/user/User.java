45a5c065ec46255e773252963f8de03559beb6d0
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-16 21:51:34 +0300

    0206-save-user-to-db

---
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,56 @@
+package com.hoaxify.ws.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    long id;
+
+    String username;
+
+    String email;
+
+    String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    
+}
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,12 +1,19 @@
 package com.hoaxify.ws.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping("/api/v1/users")
-    void createUser(){}
+    void createUser(@RequestBody User user){
+        userRepository.save(user);
+    }
     
 }
