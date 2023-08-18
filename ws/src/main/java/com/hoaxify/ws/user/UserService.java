55b655b6b85b8e7a652c3db3276f12f1e34f4bf7
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-18 23:12:10 +0300

    0502-send-mail

--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,9 +1,13 @@
 package com.hoaxify.ws.user;
 
+import java.util.Properties;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,9 +27,32 @@ public class UserService {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setActivationToken(UUID.randomUUID().toString());
             userRepository.save(user);
+            sendActivationEmail(user);
         } catch (DataIntegrityViolationException ex){
             throw new NotUniqueEmailException();
         }
+    }
+
+    private void sendActivationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@my-app.com");
+        message.setTo(user.getEmail());
+        message.setSubject("Account Activation");
+        message.setText("http://localhost:5173/activation/"+user.getActivationToken());
+        getJavaMailSender().send(message);
+    }
+
+    public JavaMailSender getJavaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.ethereal.email");
+        mailSender.setPort(587);
+        mailSender.setUsername("isaac.goyette73@ethereal.email");
+        mailSender.setPassword("BFfKYVxxnHH2QcT71P");
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        return mailSender;
+
     }
     
 }
