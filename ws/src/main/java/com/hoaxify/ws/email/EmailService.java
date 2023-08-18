1f3219fe7d57f7f1a92a9da2b290aeff1426f76f
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-19 01:00:03 +0300

    0510-localized-html-mail-content

--- ws/src/main/java/com/hoaxify/ws/email/EmailService.java
+++ ws/src/main/java/com/hoaxify/ws/email/EmailService.java
@@ -3,13 +3,17 @@ package com.hoaxify.ws.email;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.hoaxify.ws.configuration.HoaxifyProperties;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -19,12 +23,11 @@ public class EmailService {
     @Autowired
     HoaxifyProperties hoaxifyProperties;
 
+    @Autowired
+    MessageSource messageSource;
+
     @PostConstruct
     public void initialize(){
-        System.err.println("--------");
-        System.err.println(hoaxifyProperties.getClient().host());
-        System.err.println("--------");
-
         this.mailSender = new JavaMailSenderImpl();
         mailSender.setHost(hoaxifyProperties.getEmail().host());
         mailSender.setPort(hoaxifyProperties.getEmail().port());
@@ -36,14 +39,38 @@ public class EmailService {
 
     }
 
+    String activationEmail = """
+            <html>
+                <body>
+                    <h1>${title}</h1>
+                    <a href="${url}">${clickHere}</a>
+                </body>
+            </html>
+            """;
+
     public void sendActivationEmail(String email, String activationToken) {
         var activationUrl = hoaxifyProperties.getClient().host() + "/activation/" + activationToken;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(hoaxifyProperties.getEmail().from());
-        message.setTo(email);
-        message.setSubject("Account Activation");
-        message.setText(activationUrl);
-        this.mailSender.send(message);
+        var title = messageSource.getMessage("hoaxify.mail.user.created.title", null, LocaleContextHolder.getLocale());
+        var clickHere = messageSource.getMessage("hoaxify.mail.click.here", null, LocaleContextHolder.getLocale());
+
+
+        var mailBody = activationEmail
+            .replace("${url}", activationUrl)
+            .replace("${title}", title)
+            .replace("${clickHere}", clickHere);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+        try {
+            message.setFrom(hoaxifyProperties.getEmail().from());
+            message.setTo(email);
+            message.setSubject(title);
+            message.setText(mailBody, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        
+        this.mailSender.send(mimeMessage);
     }
 
     
