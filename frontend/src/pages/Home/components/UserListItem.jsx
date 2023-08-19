71bfeea20c29f4d8e0820db888dd032ea887d555
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-19 23:29:50 +0300

    0711-default-profile-image

--- frontend/src/pages/Home/components/UserListItem.jsx
+++ frontend/src/pages/Home/components/UserListItem.jsx
@@ -1,8 +1,14 @@
+import defaultProfileImage from "@/assets/profile.png";
+
 export function UserListItem({ user }) {
   return (
-    <li 
-    className="list-group-item list-group-item-action" >
-        {user.username}
+    <li className="list-group-item list-group-item-action">
+      <img
+        src={defaultProfileImage}
+        width="30"
+        className="img-fluid rounded-circle shadow-sm"
+      />
+      <span className="ms-2">{user.username}</span>
     </li>
   );
 }
