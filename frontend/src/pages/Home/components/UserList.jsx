32a93b558cceeb44569adcd6dcb81a4e2faf848a
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-19 22:41:44 +0300

    0706-style-n-progress

--- frontend/src/pages/Home/components/UserList.jsx
+++ frontend/src/pages/Home/components/UserList.jsx
@@ -1,31 +1,62 @@
 import { useCallback, useEffect, useState } from "react";
 import { loadUsers } from "./api";
+import { Spinner } from "@/shared/components/Spinner";
 
 export function UserList() {
   const [userPage, setUserPage] = useState({
-    content: [],
+    content: [{ username: "test" }],
     last: false,
     first: false,
-    number: 0
+    number: 0,
   });
+  const [apiProgress, setApiProgress] = useState(false);
 
   const getUsers = useCallback(async (page) => {
+    setApiProgress(true);
+    try {
       const response = await loadUsers(page);
       setUserPage(response.data);
-  }, [])
+    } catch {
+    } finally {
+      setApiProgress(false)
+    }
+  }, []);
 
   useEffect(() => {
     getUsers();
   }, []);
 
   return (
-    <>
-      <div>User List</div>
-      {userPage.content.map((user) => {
-        return <div>{user.username}</div>;
-      })}
-      {!userPage.first && <button onClick={() => getUsers(userPage.number - 1)}>Previous</button>}
-      {!userPage.last && <button onClick={() => getUsers(userPage.number + 1)}>Next</button>}
-    </>
+    <div className="card">
+      <div className="card-header text-center fs-4">User List</div>
+      <ul className="list-group list-group-flush">
+        {userPage.content.map((user) => {
+          return (
+            <li className="list-group-item list-group-item-action">
+              {user.username}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="card-footer text-center">
+        {apiProgress && <Spinner />}
+        {!apiProgress && !userPage.first && (
+          <button
+            className="btn btn-outline-secondary btn-sm float-start"
+            onClick={() => getUsers(userPage.number - 1)}
+          >
+            Previous
+          </button>
+        )}
+        {!apiProgress && !userPage.last && (
+          <button
+            className="btn btn-outline-secondary btn-sm float-end"
+            onClick={() => getUsers(userPage.number + 1)}
+          >
+            Next
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
