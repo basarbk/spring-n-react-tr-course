d22d486445e5b9b4958902615d874e94054d0c91
Basar Buyukkahraman <basarbk@yahoo.com>
2023-08-20 13:16:53 +0300

    0905-refacor-error-handler

--- ws/src/main/java/com/hoaxify/ws/error/ErrorHandler.java
+++ ws/src/main/java/com/hoaxify/ws/error/ErrorHandler.java
@@ -21,65 +21,38 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<ApiError> handleMethodArgNotValidEx(MethodArgumentNotValidException exception, HttpServletRequest request){
-        ApiError apiError = new ApiError();
-        apiError.setPath(request.getRequestURI());
-        String message = Messages.getMessageForLocale("hoaxify.error.validation", LocaleContextHolder.getLocale());
-        apiError.setMessage(message);
-        apiError.setStatus(400);
-        var validationErrors = exception.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (existing, replacing) -> existing));
-        apiError.setValidationErrors(validationErrors);
-        return ResponseEntity.badRequest().body(apiError);
-    }
-
-
-
-    @ExceptionHandler(NotUniqueEmailException.class)
-    ResponseEntity<ApiError> handleNotUniqueEmailEx(NotUniqueEmailException exception){
-        ApiError apiError = new ApiError();
-        apiError.setPath("/api/v1/users");
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(400);
-        apiError.setValidationErrors(exception.getValidationErrors());
-        return ResponseEntity.status(400).body(apiError);
-    }
-
-    @ExceptionHandler(ActivationNotificationException.class)
-    ResponseEntity<ApiError> handleActivationNotificationException(ActivationNotificationException exception){
-        ApiError apiError = new ApiError();
-        apiError.setPath("/api/v1/users");
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(502);
-        return ResponseEntity.status(502).body(apiError);
-    }
-
-    @ExceptionHandler(InvalidTokenException.class)
-    ResponseEntity<ApiError> handleInvalidTokenException(InvalidTokenException exception, HttpServletRequest request){
+    @ExceptionHandler({
+        MethodArgumentNotValidException.class,
+        NotUniqueEmailException.class,
+        ActivationNotificationException.class,
+        InvalidTokenException.class,
+        NotFoundException.class,
+        AuthenticationException.class
+    })
+    ResponseEntity<ApiError> handleException(Exception exception, HttpServletRequest request){
         ApiError apiError = new ApiError();
         apiError.setPath(request.getRequestURI());
         apiError.setMessage(exception.getMessage());
-        apiError.setStatus(400);
-        return ResponseEntity.status(400).body(apiError);
-    }
+        if(exception instanceof MethodArgumentNotValidException) {
+            String message = Messages.getMessageForLocale("hoaxify.error.validation", LocaleContextHolder.getLocale());
+            apiError.setMessage(message);
+            apiError.setStatus(400);
+            var validationErrors = ((MethodArgumentNotValidException)exception).getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (existing, replacing) -> existing));
+            apiError.setValidationErrors(validationErrors);
+        } else if (exception instanceof NotUniqueEmailException) {
+            apiError.setStatus(400);
+            apiError.setValidationErrors(((NotUniqueEmailException)exception).getValidationErrors());
+        } else if (exception instanceof ActivationNotificationException) {
+            apiError.setStatus(502);
+        } else if (exception instanceof InvalidTokenException) {
+            apiError.setStatus(400);
+        } else if (exception instanceof NotFoundException) {
+            apiError.setStatus(404);
+        } else if (exception instanceof AuthenticationException) {
+            apiError.setStatus(401);
+        } 
 
-    @ExceptionHandler(NotFoundException.class)
-    ResponseEntity<ApiError> handleNotFoundException(NotFoundException exception, HttpServletRequest request){
-        ApiError apiError = new ApiError();
-        apiError.setPath(request.getRequestURI());
-        apiError.setMessage(exception.getMessage());
-        apiError.setStatus(404);
-        return ResponseEntity.status(404).body(apiError);
-    }
-
-    
-    @ExceptionHandler(AuthenticationException.class)
-    ResponseEntity<?> handleAuthenticationException(AuthenticationException exception){
-        ApiError error = new ApiError();
-        error.setPath("/api/v1/auth");
-        error.setStatus(401);
-        error.setMessage(exception.getMessage());
-        return ResponseEntity.status(401).body(error);
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
     
 }
